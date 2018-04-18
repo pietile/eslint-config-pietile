da2de7806d5063b346f646df97fe3c523c4086aa
Maksim Milyutin <mmxckk@gmail.com>
2018-04-18 21:31:13 +0700

    Split rules to separate file

--- packages/eslint-config-pietile-base/index.js
+++ packages/eslint-config-pietile-base/index.js
@@ -1,15 +1,7 @@
+const rules = require('./rules');
+
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb-base', 'prettier'],
-  rules: {
-    'arrow-body-style': [0, 'as-needed'],
-    'class-methods-use-this': 'off',
-    'import/prefer-default-export': 'off',
-    'max-len': ['error', { code: 100, ignoreUrls: true }],
-    'no-console': 'off',
-    'no-continue': 'off',
-    'no-plusplus': 'off',
-    'no-underscore-dangle': 'off',
-    'no-use-before-define': ['error', { variables: false }],
-  },
+  rules,
 };
