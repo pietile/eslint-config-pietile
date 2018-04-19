1192161b5c07076cc20c2e5f33ddcee8a1cd0a13
Maksim Milyutin <mmxckk@gmail.com>
2018-04-19 10:27:55 +0700

    Use objectAssign polyfill instead of spread operator

--- packages/eslint-config-pietile/index.js
+++ packages/eslint-config-pietile/index.js
@@ -1,13 +1,13 @@
 const baseRules = require('eslint-config-pietile-base/rules');
+const objectAssign = require('object-assign');
 
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb', 'prettier', 'prettier/react'],
-  rules: {
-    ...baseRules,
+  rules: objectAssign({}, baseRules, {
     'react/forbid-prop-types': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/no-typos': 'off',
     'react/require-default-props': 'off',
-  },
+  }),
 };
