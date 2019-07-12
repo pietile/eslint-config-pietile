5b1678f0274f2a239a82808af3741881e4139318
Maksim Milyutin <mmxckk@gmail.com>
2019-07-12 19:38:27 +0300

    Update deps. Clean up.

--- packages/eslint-config-pietile-base/index.js
+++ packages/eslint-config-pietile-base/index.js
@@ -1,7 +1,15 @@
-const rules = require('./rules');
-
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb-base', 'prettier'],
-  rules,
+  rules: {
+    'arrow-body-style': [0, 'as-needed'],
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', { code: 100, ignoreUrls: true }],
+    'no-console': 'off',
+    'no-continue': 'off',
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': ['error', { variables: false }],
+  },
 };
